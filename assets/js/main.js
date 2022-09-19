0efe8a8570727e43e62c07ed869910f162c21fdd
sarathejohn <sarathejohn@gmail.com>
2022-09-19 15:16:36 +0530

    first second

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,6 @@
-/**
-* Template Name: Nova - v1.1.0
-* Template URL: https://bootstrapmade.com/nova-bootstrap-business-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
 document.addEventListener('DOMContentLoaded', () => {
   "use strict";
 
-  /**
-   * Preloader
-   */
-  const preloader = document.querySelector('#preloader');
-  if (preloader) {
-    window.addEventListener('load', () => {
-      preloader.remove();
-    });
-  }
 
   /**
    * Sticky header on scroll
@@ -142,42 +127,44 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 
+
   /**
    * Porfolio isotope and filter
    */
-  let portfolionIsotope = document.querySelector('.portfolio-isotope');
+   let portfolionIsotope = document.querySelector('.portfolio-isotope');
 
-  if (portfolionIsotope) {
-
-    let portfolioFilter = portfolionIsotope.getAttribute('data-portfolio-filter') ? portfolionIsotope.getAttribute('data-portfolio-filter') : '*';
-    let portfolioLayout = portfolionIsotope.getAttribute('data-portfolio-layout') ? portfolionIsotope.getAttribute('data-portfolio-layout') : 'masonry';
-    let portfolioSort = portfolionIsotope.getAttribute('data-portfolio-sort') ? portfolionIsotope.getAttribute('data-portfolio-sort') : 'original-order';
-
-    window.addEventListener('load', () => {
-      let portfolioIsotope = new Isotope(document.querySelector('.portfolio-container'), {
-        itemSelector: '.portfolio-item',
-        layoutMode: portfolioLayout,
-        filter: portfolioFilter,
-        sortBy: portfolioSort
-      });
-
-      let menuFilters = document.querySelectorAll('.portfolio-isotope .portfolio-flters li');
-      menuFilters.forEach(function(el) {
-        el.addEventListener('click', function() {
-          document.querySelector('.portfolio-isotope .portfolio-flters .filter-active').classList.remove('filter-active');
-          this.classList.add('filter-active');
-          portfolioIsotope.arrange({
-            filter: this.getAttribute('data-filter')
-          });
-          if (typeof aos_init === 'function') {
-            aos_init();
-          }
-        }, false);
-      });
-
-    });
-
-  }
+   if (portfolionIsotope) {
+ 
+     let portfolioFilter = portfolionIsotope.getAttribute('data-portfolio-filter') ? portfolionIsotope.getAttribute('data-portfolio-filter') : '*';
+     let portfolioLayout = portfolionIsotope.getAttribute('data-portfolio-layout') ? portfolionIsotope.getAttribute('data-portfolio-layout') : 'masonry';
+     let portfolioSort = portfolionIsotope.getAttribute('data-portfolio-sort') ? portfolionIsotope.getAttribute('data-portfolio-sort') : 'original-order';
+ 
+     window.addEventListener('load', () => {
+       let portfolioIsotope = new Isotope(document.querySelector('.portfolio-container'), {
+         itemSelector: '.portfolio-item',
+         layoutMode: portfolioLayout,
+         filter: portfolioFilter,
+         sortBy: portfolioSort
+       });
+ 
+       let menuFilters = document.querySelectorAll('.portfolio-isotope .portfolio-flters li');
+       menuFilters.forEach(function(el) {
+         el.addEventListener('click', function() {
+           document.querySelector('.portfolio-isotope .portfolio-flters ').classList.remove('filter-active');
+           this.classList.add('filter-active');
+           portfolioIsotope.arrange({
+             filter: this.getAttribute('data-filter')
+           });
+           if (typeof aos_init === 'function') {
+             aos_init();
+           }
+         }, false);
+       });
+ 
+     });
+ 
+   }
+ 
 
   /**
    * Animation on scroll function and init
@@ -195,3 +182,5 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
 });
+
+// 
